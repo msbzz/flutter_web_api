f4d7915a46a08a79f0049e4e936b43e1b777047d
Marco <mbarozzi@gmail.com>
2024-08-16 15:27:49 -0300

    projeto base

---
+++ bytebank/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:bytebank/screens/dashboard.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(BytebankApp());
+}
+
+class BytebankApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+return MaterialApp(
+  theme: ThemeData(
+    colorScheme: ColorScheme.fromSwatch().copyWith(
+      primary: Colors.green[900],
+      secondary: Colors.blueAccent[700], // Substitui accentColor
+    ),
+    scaffoldBackgroundColor: Colors.white,
+    buttonTheme: ButtonThemeData(
+      buttonColor: Colors.blueAccent[700],
+      textTheme: ButtonTextTheme.primary,
+    ),
+  ),
+  home: Dashboard(),
+);
+  }
+}
