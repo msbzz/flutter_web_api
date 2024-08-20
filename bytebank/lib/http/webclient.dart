15ca77bbc5c574640511dd0786d8268a739bf33c
Marco <mbarozzi@gmail.com>
2024-08-20 03:28:01 -0300

    implementando metodo save

--- bytebank/lib/http/webclient.dart
+++ bytebank/lib/http/webclient.dart
@@ -1,27 +1,63 @@
 import 'dart:convert';
+import 'dart:ffi';
 
+import 'package:bytebank/models/contact.dart';
 import 'package:bytebank/models/transanction.dart';
 import 'package:http/http.dart';
 
 import 'package:http_interceptor/http_interceptor.dart';
 
+Client client = InterceptedClient.build(interceptors: [
+  LoggingInterceptor(),
+]);
+
+final Uri url = Uri.parse('http://192.168.0.64:8080/transactions');
+
 Future<List<Transaction>> findAll() async {
-  Client client = InterceptedClient.build(interceptors: [
-    LoggingInterceptor(),
-  ]); 
-  final Uri url = Uri.parse('http://192.168.0.64:8080/transactions');
- 
   final Response response = await client.get(url).timeout(Duration(seconds: 4));
 
   final List<dynamic> decodedJson = jsonDecode(response.body);
-  
+
   final List<Transaction> transactions = [];
 
   for (var json in decodedJson) {
     transactions.add(Transaction.fromJson(json));
   }
-  
-  return transactions;  
+
+  return transactions;
+}
+
+Future<Transaction> save(Transaction transaction) async {
+  final Map<String, dynamic> transactionMap = {
+    'value': transaction.value,
+    'contact': {
+      'name': transaction.contact.name,
+      'accountNumber': transaction.contact.accountNumber
+    }
+  };
+
+  final String transactionJson = jsonEncode(transactionMap);
+
+  final response = await client.post(url,
+      headers: {'Content-type': 'application/json', 'password': '1000'},
+      body: transactionJson);
+
+  print('>>> response <<<');
+  print('>>> ${response.toString()} <<<');
+
+  Map<String, dynamic> responseJson = jsonDecode(response.body);
+  final Map<String, dynamic> contactJson = responseJson['contact'];
+
+  print('contactJson >>> ${contactJson.toString()}');
+
+  return Transaction(
+    responseJson['value'],
+    Contact(
+      0,
+      contactJson['name'],
+      contactJson['accountNumber'],
+    ),
+  );
 }
 
 class LoggingInterceptor implements InterceptorContract {
@@ -34,7 +70,8 @@ class LoggingInterceptor implements InterceptorContract {
   @override
   Future<ResponseData> interceptResponse({required ResponseData data}) async {
     if (data.statusCode >= 400) {
-      throw HttpExceptionWithStatus(data.statusCode,  data.body ?? 'Erro desconhecido');
+      throw HttpExceptionWithStatus(
+          data.statusCode, data.body ?? 'Erro desconhecido');
     }
     return data;
   }
--- bytebank/lib/main.dart
+++ bytebank/lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:bytebank/http/webclient.dart';
+import 'package:bytebank/models/contact.dart';
+import 'package:bytebank/models/transanction.dart';
 import 'package:bytebank/screens/dashboard.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  save(Transaction(350.0,Contact(0,'marcela',4000))).then((transaction)=>print('>>> adicionado : ${transaction.toString()}  <<<'));
   //findAll().then((transactions)=> print('new transactions ${transactions}'));
   runApp(BytebankApp());
 }
